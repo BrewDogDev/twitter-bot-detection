0ca97354ded28ec0422e0ddbd910b2d43075cc90
Allan Pedin <allan.pedin.17@cnu.edu>
2021-04-07 09:42:53 -0400

    env work

--- src/data/twitter.py
+++ src/data/twitter.py
@@ -1,1 +1,9 @@
 import tweepy
+
+import osfrom dotenv import load_dotenv
+
+load_dotenv()
+
+GCP_PROJECT_ID = os.getenv('')
+SERVICE_ACCOUNT_FILE = os.getenv('SERVICE_ACCOUNT_FILE')
+STORAGE_BUCKET_NAME = os.getenv('STORAGE_BUCKET_NAME')
